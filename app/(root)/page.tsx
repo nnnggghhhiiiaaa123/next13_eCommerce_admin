8bb6f6f447c2cf54b8b9e579bc6473f6862fa772
chien <chiennguyen.170921@gmail.com>
2023-09-22 13:51:17 +0700

    chore: add .env.example file
    chore: add .env to .gitignore
    
    feat: add sign-in page
    
    feat: add sign-up page
    
    feat: add AuthLayout component
    
    refactor: rename Home component to SetupPage
    
    feat: add ClerkProvider to RootLayout component
    
    feat: add middleware for authentication
    
    chore: add @clerk/nextjs dependency to package.json

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs"
 
-export default function Home() {
+const SetupPage = () => {
     return (
         <div className="p-4">
-            <Button>
-                Click Me
-            </Button>
+        <UserButton  afterSignOutUrl="/"/>
         </div>
     )
 }
+export default SetupPage
