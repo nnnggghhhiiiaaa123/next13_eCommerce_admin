a9a5b4e88ba863ea791e75cf801ca34e3fc7b844
chien <chiennguyen.170921@gmail.com>
2023-09-22 16:23:39 +0700

    chore(package.json): update dependencies feat(page.tsx): remove unnecessary whitespace feat(store-modal.tsx): add form validation using zod schema feat(form.tsx): add form components for building forms feat(input.tsx): add Input component for form inputs feat(label.tsx): add Label component for form labels

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,6 +14,7 @@ const SetupPage = () => {
             onOpen();
         }
     }, [isOpen, onOpen]);
+    
     return (
         <div className="p-4">
             {/* <UserButton afterSignOutUrl="/" /> */}
