e24881218661160aa3aeb69d7431ce766c75359f
Cotchi666 <chien.170901@gmail.com>
2023-10-10 16:44:55 +0700

    refactor(dialog.tsx): rename DialogPrimitive.Portal to SheetPortal for improved readability and semantics fix(dialog.tsx): update DialogPortal's displayName to match the renamed SheetPortal's displayName

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -7,7 +7,7 @@ import { X } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 const Dialog = DialogPrimitive.Root;
-
+const SheetPortal = DialogPrimitive.Portal;
 const DialogTrigger = DialogPrimitive.Trigger;
 
 const DialogPortal = ({
@@ -15,13 +15,13 @@ const DialogPortal = ({
   children,
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props}>
+  <SheetPortal className={cn(className)} {...props}>
     <div className="fixed inset-0 z-50 flex items-start justify-center sm:items-center">
       {children}
     </div>
-  </DialogPrimitive.Portal>
+  </SheetPortal>
 );
-DialogPortal.displayName = DialogPrimitive.Portal.displayName;
+DialogPortal.displayName = SheetPortal.displayName;
 
 const DialogOverlay = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
